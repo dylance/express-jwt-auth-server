09b01b4c2cf90922bc3f7e5df2d86bbe9fcc00c3
Dylan Ellison <dylancellison@gmail.com>
2019-10-23 23:34:39 -0500

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const keys = require('../config/keys');
+const router = require('./router');
+
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const app = express();
+
+app.use(morgan('combined'));
+
+// attempts to parse any request into JSON
+app.use(bodyParser.json({ type: '*/*' }));
+
+router(app);
+
+app.listen(5000, () => console.log('Listening on port 5000'));
